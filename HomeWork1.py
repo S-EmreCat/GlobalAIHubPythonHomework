6ecbbfe371e599bb0a26abb2228c53bcbf2502ac
S-EmreCat <emre__cat@outlook.com>
2020-12-26 11:17:51 +0300

    first commit

---
+++ HomeWork1.py
@@ -0,0 +1,8 @@
+value_1= input("Please enter value 1: ")
+value_2= input("Please enter value 2: ")
+value_3= input("Please enter value 3: ")
+value_4= input("Please enter value 4: ")
+value_5= input("Please enter value 5: ")
+
+print(f'"Value_1_type: "{type(value_1)}\n"Value_2_type: "{type(value_2)}\n"Value_3_type: "{type(value_3)}\n"Value_4_type: "{type(value_4)}\n"Value_5_type: "{type(value_5)}\n')
+

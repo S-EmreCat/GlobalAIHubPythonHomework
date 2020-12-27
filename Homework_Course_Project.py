88049efa0ff6c230ee739a44a154815ff48e0acd
S-EmreCat <emre__cat@outlook.com>
2020-12-27 14:52:46 +0300

    proje bitti

--- Homework_Course_Project.py
+++ Homework_Course_Project.py
@@ -1,4 +1,6 @@
 import sys
+
+
 # ogrenci girisi için gerekli işlemler
 def Student_SignIn():
     name='Saban Emre'
@@ -16,28 +18,31 @@ def Student_SignIn():
             if count==0:
                 print("Please try again later")
                 sys.exit()
-#kullanıcıdan derslerin alınıp liste oluşturulması
-def Lessons(*args):
-    lessons=[]
-    for arg in args:
-        lessons.append(arg)
-    # print(lessons)
-    # print(len(lessons))
-    return(len(lessons))
-
+# kullanıcıdan derslerin alınıp liste oluşturulması
 # almak istenilen ders sayısı foknsiyona gönderilir.
-def Lessons_Selected(numberOfLessons):
+def Lessons_Selected(numberOfLessons,ders_listesi):
     selected_lessons=[]
-    lessons_value=Lessons('lesson 1','lesson 2','lesson 3','lesson 4','lesson 5')
+    all_lessons=[]
+    all_lessons=ders_listesi
+    # for arg in args:
+    #     all_lessons.append(arg)
+    len_lessons=len(all_lessons)
+    # print("ders sayısı: ",len_lessons)
     if numberOfLessons<3:
         raise "You failed in class"
-    elif 3<=lessons_value<=5:
+    elif 3<=len_lessons<=5:
         print('*'*30+"\n"+"Selecting your lessons.\n"+'*'*30)
         for i in range(numberOfLessons):
-            selected_lessons.append('lesson '+str(i+1))
-        print(selected_lessons)
+            try:
+                selected_lessons.append(all_lessons[i])
+            except:
+                raise "Wrong Choice. Try less than 5"
+        print(f"Selected Lessons: {selected_lessons} ")
+    
+    return selected_lessons
 
-def not_hesapla():
+# dersin harf notunu hesaplar
+def Calculator_Grade():
     Exam_Notes={"midterm":'98',"final": "100","proje": "89"}
     midterm=int(Exam_Notes['midterm'])
     proje=int(Exam_Notes['proje'])
@@ -53,6 +58,23 @@ def not_hesapla():
         print(f"Grade is {grade} DD")   
     elif grade<30:
         print(f"Grade is ({grade}) less than 30 (FF).")
+
+
+dersler=[]
 Student_SignIn()
-Lessons_Selected(3)
-not_hesapla()
+    
+for i in range(5):
+    ders=input(f"Course {i+1} name: ")
+    dersler.append(ders)
+
+print(f"All Lessons: ",dersler)
+
+numberofLessons=int(input("Enter the number of courses you want to take: "))
+
+Lessons_Selected(numberOfLessons=numberofLessons,ders_listesi=dersler)
+
+harfnotu_dersi=input("Enter the lesson you want to learn your grade for: ")
+Calculator_Grade()
+
+
+
